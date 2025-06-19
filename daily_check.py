0b4bd01505441770109a910421c5463bf930792f
Enzoki <51037572+Enzoki@users.noreply.github.com>
2025-06-19 11:36:29 +0800

    Update daily_check.py

--- daily_check.py
+++ daily_check.py
@@ -1,4 +1,4 @@
-from main import get_travel_advisory, send_telegram_message, generate_message, TOKEN, CHANNEL
+from main import get_travel_advisory, send_discord_message, generate_message
 import datetime as dt
 import asyncio
 import os
@@ -7,7 +7,7 @@ import sys
 async def main():
     travel_adv = get_travel_advisory()
     message = generate_message(travel_adv)
-    await send_telegram_message(TOKEN, CHANNEL, message)
+    await send_discord_message(TOKEN, CHANNEL, message)
 
 if __name__ == "__main__":
     asyncio.run(main())
