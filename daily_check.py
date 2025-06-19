fa1221cc494dbdef4dbc724ad7449d56e338a970
Enzoki <51037572+Enzoki@users.noreply.github.com>
2025-06-19 11:42:05 +0800

    Update daily_check.py

--- daily_check.py
+++ daily_check.py
@@ -4,10 +4,12 @@ import asyncio
 import os
 import sys
 
+DISCORD_WEBHOOK_URL = os.environ.get('DISCORD_WEBHOOK_URL')
+
 async def main():
     travel_adv = get_travel_advisory()
     message = generate_message(travel_adv)
-    await send_discord_message(TOKEN, CHANNEL, message)
+    await send_discord_message(DISCORD_WEBHOOK_URL, message)
 
 if __name__ == "__main__":
     asyncio.run(main())
